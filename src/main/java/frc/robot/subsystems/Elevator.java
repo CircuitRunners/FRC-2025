e604c373c287f40b56a2cfc83078f26373319704
ethalandzia <ethalandzia@gmail.com>
2025-02-14 17:32:47 -0500

    continued working on elevator, connected elevator to controller

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -4,6 +4,7 @@ import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Elevator extends SubsystemBase {
@@ -16,7 +17,19 @@ public class Elevator extends SubsystemBase {
     public Elevator(){
         elevatorSparkMax1 = new SparkMax(20, MotorType.kBrushless);
         elevatorSparkMax2 = new SparkMax(21, MotorType.kBrushless);
+        
         elevatorEncoder = elevatorSparkMax1.getEncoder();
         
+    }
+
+    public void moveToPos (double targetPos) {
+        double kp = 1;
+        double ki = 0;
+        double kd = 0;
+
+        ProfiledPIDController elevatorPidController = new ProfiledPIDController(kp, ki, kd, null);
+
+        elevatorSparkMax1.set(elevatorPidController.calculate(elevatorEncoder.getPosition(), targetPos));
+        elevatorSparkMax2.set(-(elevatorPidController.calculate(elevatorEncoder.getPosition(),targetPos)));
     }
 }
