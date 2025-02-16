d1b59014fc667b95fa19c474c2c5218b56d89abe
danthaiP <daniel.thai@wheelermagnet.com>
2025-02-16 14:33:42 -0500

    created claw.java

---
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -0,0 +1,39 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.Command;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+public class Claw extends SubsystemBase {
+    
+    private SparkMax leftMotor;
+    private SparkMax rightMotor;
+
+    public Claw() {
+        leftMotor = new SparkMax(24, MotorType.kBrushless);
+        rightMotor = new SparkMax(25, MotorType.kBrushless);
+    }
+
+    public void grab() {
+        leftMotor.set(1);
+        rightMotor.set(-1);
+    }
+
+    public void release() {
+        leftMotor.set(1);
+        rightMotor.set(-1);
+    }
+
+    public Command grabCommand() {
+        return run(() -> grab());
+    }
+
+    public Command releaseCommand() {
+        return run(() -> release());
+    }
+
+
+
+}
