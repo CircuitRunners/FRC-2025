bdb82e1a10db29b3085e045426a4c06a2aa5702d
Goodnews <kegovin2823@gmail.com>
2025-02-16 18:11:52 -0500

    Removed trapezoidal profiling and used a regular PID controller. Also fixed the ports to correct numbers

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -4,10 +4,9 @@ import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
-import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Elevator extends SubsystemBase {
@@ -15,31 +14,29 @@ public class Elevator extends SubsystemBase {
     public final SparkMax elevatorSparkMax1;
     public final SparkMax elevatorSparkMax2;
     private final RelativeEncoder elevatorEncoder;
-
-    private final ProfiledPIDController elevatorPidController;
+    private final PIDController pidController;
 
     public Elevator(){
-        //Initialize the two motors
-        elevatorSparkMax1 = new SparkMax(20, MotorType.kBrushless);
-        elevatorSparkMax2 = new SparkMax(21, MotorType.kBrushless);
+        //Initialize motors with correct ports
+        elevatorSparkMax1 = new SparkMax(21, MotorType.kBrushless);
+        elevatorSparkMax2 = new SparkMax(22, MotorType.kBrushless);
         
         elevatorEncoder = elevatorSparkMax1.getEncoder();
 
-        //Tunes the PID gains -Adjusted for better control and movement of elevator
+        //Tunes the PID gains- Adjust for better control and movement of elevator
         double kp = 1.0; //Proportional (Increase the number if moving too slow, decrease if oscillating)
         double ki = 0.0; //Integral (Stays at 0 unless there's a steady-state error)
         double kd = 0.01; //Derivate (Increase if overshoot target, decrease if sluggish/slow)
 
-        //Motion constraints
-        TrapezoidProfile.Constraints constraints = new TrapezoidProfile.Constraints(2000, 1500);
         
         //Initialize PID controller with motion constraints
-        elevatorPidController = new ProfiledPIDController(kp, ki, kd, constraints);
-        elevatorPidController.setTolerance(0.5); //Acceptable error range
+        pidController = new PIDController(kp, ki, kd);
+        pidController.setTolerance(0.5); //Acceptable error range
     }
 
-    public void moveToPos (double targetPos) {  
-        elevatorSparkMax1.set(elevatorPidController.calculate(elevatorEncoder.getPosition(), targetPos));
+    public void moveToPos (double targetPos) {
+  
+        elevatorSparkMax1.set(pidController.calculate(elevatorEncoder.getPosition(), targetPos));
     }
 
     public double getPos () {
