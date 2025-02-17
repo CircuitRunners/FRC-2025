cc42a363049b0dda20f753e184b90f397e9629ba
danthaiP <daniel.thai@wheelermagnet.com>
2025-02-16 23:14:53 -0500

    reworked claw.java bc im stupid

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -6,32 +6,53 @@ import edu.wpi.first.wpilibj2.command.Command;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+// ngl idk what am doing so am stealing from test code
+
 public class Claw extends SubsystemBase {
     
-    private SparkMax leftMotor;
-    private SparkMax rightMotor;
+    private SparkMax moveMotor;
+    private SparkMax rollerMotor;
 
     public Claw() {
-        leftMotor = new SparkMax(24, MotorType.kBrushless);
-        rightMotor = new SparkMax(25, MotorType.kBrushless);
+        moveMotor = new SparkMax(24, MotorType.kBrushless);
+        rollerMotor = new SparkMax(25, MotorType.kBrushless);
     }
 
-    public void grab() {
-        leftMotor.set(1);
-        rightMotor.set(-1);
+    public void moveClaw(double speed) {
+        moveMotor.set(speed);
     }
 
-    public void release() {
-        leftMotor.set(-1);
-        rightMotor.set(1);
+    public void changeRollerSpd(double speed) {
+        rollerMotor.set(speed);
     }
 
-    public Command grabCommand() {
-        return run(() -> grab());
+    public void stopClaw() {
+        moveMotor.stopMotor();
     }
 
-    public Command releaseCommand() {
-        return run(() -> release());
+    public void stopRoller() {
+        rollerMotor.stopMotor();
+    }
+
+    public Command moveClawCommand(double speed) {
+        return run(() -> moveClaw(speed));
+    }
+
+    public Command changeRollerSpdCommand(double speed) {
+        return run(() -> changeRollerSpd(speed));
+    }
+
+    public Command stopClawCommand() {
+        return run(this::stopClaw);
+    }
+
+    public Command stopRollerCommand() {
+        return run(this::stopRoller);
+    }
+
+    @Override
+    public void periodic() {
+        // icl ts pmo sm u bfr rn
     }
 
 
