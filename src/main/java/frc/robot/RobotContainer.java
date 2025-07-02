55da7ca2180ba14db4980b4ff3fa8ddbfee40cd7
Y0ur-dad <tarun.adi@wheelermagnet.com>
2025-07-02 15:48:59 -0400

    Fixed Joystick back to Xbox controller in RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -37,7 +37,7 @@ public class RobotContainer
 {
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  final         CommandJoystick driverXbox = new CommandJoystick(0);
+  final         CommandXboxController driverXbox = new CommandXboxController(0);
   // The robot's subsystems and commands are defined here...
   public final SwerveSubsystem       drivebase  = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
                                                                                 "swerve/neo"));
