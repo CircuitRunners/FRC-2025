4f0e12d97677d3592b06a927e8e6736fff938ef9
ian <ianamerault@gmail.com>
2025-10-03 21:23:51 -0400

    jiggles if coral gets stuck

--- src/main/java/frc/robot/commands/auton/TripleScore.java
+++ src/main/java/frc/robot/commands/auton/TripleScore.java
@@ -2,6 +2,7 @@ package frc.robot.commands.auton;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.moving.MoveToIntake;
@@ -19,17 +20,17 @@ public class TripleScore extends SequentialCommandGroup{
         addCommands(
         new AutonScoreL43Coral(drive, elevator, claw, left),
         new ParallelCommandGroup(drive.PPHPAlign(), new MoveToIntake(elevator, claw, drive)),
-        new SequentialCommandGroup(drive.driveRobotCentricCommand(() -> new ChassisSpeeds(0, 0, 0)).withTimeout(0.05).andThen(claw.autoIntakeCommand().until(() -> claw.isCoralInClaw()).withTimeout(1.5)).andThen(claw.runRollersInCommand().withTimeout(.2).finallyDo(claw::stopRoller))),
+        new SequentialCommandGroup(drive.driveRobotCentricCommand(() -> new ChassisSpeeds(0, 0, 0)).withTimeout(0.05).andThen(claw.autoIntakeCommand().until(() -> claw.isCoralInClaw()).withTimeout(1.5)).andThen(Commands.either(claw.runRollersInCommand().withTimeout(.2), jiggle().repeatedly().until(claw::isCoralInClaw).andThen(claw.runRollersInCommand().withTimeout(.2)), claw::isCoralInClaw)).finallyDo(claw::stopRoller)),
         new AutonScoreL43Coral(drive, elevator, claw, true),
         new ParallelCommandGroup(drive.PPHPAlign(), new MoveToIntake(elevator, claw, drive)),
-        new SequentialCommandGroup(drive.driveRobotCentricCommand(() -> new ChassisSpeeds(0, 0, 0)).withTimeout(0.05).andThen(claw.autoIntakeCommand().until(() -> claw.isCoralInClaw()).withTimeout(1.5)).andThen(claw.runRollersInCommand().withTimeout(.2).finallyDo(claw::stopRoller))),
+        new SequentialCommandGroup(drive.driveRobotCentricCommand(() -> new ChassisSpeeds(0, 0, 0)).withTimeout(0.05).andThen(claw.autoIntakeCommand().until(() -> claw.isCoralInClaw()).withTimeout(1.5)).andThen(Commands.either(claw.runRollersInCommand().withTimeout(.2), jiggle().repeatedly().until(claw::isCoralInClaw).andThen(claw.runRollersInCommand().withTimeout(.2)), claw::isCoralInClaw)).finallyDo(claw::stopRoller)),
         new AutonScoreL43Coral(drive, elevator, claw, false),
         new MoveToIntake(elevator, claw, drive)
         );
     }
     public Command jiggle() {
-        return drive.driveRobotCentricCommand(() -> ChassisSpeeds.fromRobotRelativeSpeeds(1, 0, 0, drive.getRotation2d())).withTimeout(0.1).andThen(drive.driveRobotCentricCommand(() -> ChassisSpeeds.fromRobotRelativeSpeeds(-1, 0, 0, drive.getRotation2d())).withTimeout(0.1));
-
+        return drive.driveRobotCentricCommand(() -> ChassisSpeeds.fromRobotRelativeSpeeds(2, 0, 0, drive.getRotation2d())).withTimeout(0.1).andThen(drive.driveRobotCentricCommand(() -> ChassisSpeeds.fromRobotRelativeSpeeds(-2, 0, 0, drive.getRotation2d())).withTimeout(0.1))
+        .andThen(drive.driveRobotCentricCommand(() -> ChassisSpeeds.fromRobotRelativeSpeeds(0, 2, 0, drive.getRotation2d())).withTimeout(0.1).andThen(drive.driveRobotCentricCommand(() -> ChassisSpeeds.fromRobotRelativeSpeeds(0, -2, 0, drive.getRotation2d())).withTimeout(0.1)));
     }
 
 }
